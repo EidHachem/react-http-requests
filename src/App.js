49f331e07b538136b5aeeebbc06c24199d24b1f3
eid <eidhachem1@gmail.com>
2024-09-13 09:01:57 +0300

    Fetch movies on component render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 
 import MoviesList from "./components/MoviesList"
 import "./App.css"
@@ -8,7 +8,11 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
-  const fetchMoviesHandler = async () => {
+  useEffect(() => {
+    fetchMoviesHandler()
+  }, [])
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true)
     try {
       const response = await fetch("https://swapi.dev/api/films/")
@@ -29,7 +33,7 @@ function App() {
       setError(error.message)
     }
     setIsLoading(false)
-  }
+  }, [])
 
   let content = <p>Found no movies.</p>
 
